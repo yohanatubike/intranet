adfebb4f1ed4e2c96ed61e6e7f93b6029ea90d95
yohana.tubike <yohana.tubike@tasac.go.tz>
2022-09-26 11:52:57 +0300

    fixed merge

--- IntranetPortal/Models/ActivitiesComment.cs
+++ IntranetPortal/Models/ActivitiesComment.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IntranetPortal.Models
 {
     public partial class ActivitiesComment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
         public long ActivitiesCommentsId { get; set; }
         public string Comment { get; set; } = null!;
         public long ActivityId { get; set; }
