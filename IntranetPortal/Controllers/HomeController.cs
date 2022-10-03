962934bce395ef67a683945132e3925db56a094a
yohana.tubike <yohana.tubike@tasac.go.tz>
2022-10-03 09:23:28 +0300

    Finalised carousel implementation

--- IntranetPortal/Controllers/HomeController.cs
+++ IntranetPortal/Controllers/HomeController.cs
@@ -36,7 +36,7 @@ namespace IntranetPortal.Controllers
         {
             using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
             {
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
                 byte[] hashBytes = md5.ComputeHash(inputBytes);
                 StringBuilder sb = new StringBuilder();
                 for (int i = 0; i < hashBytes.Length; i++)
